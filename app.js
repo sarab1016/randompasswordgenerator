348f0a4173f06b4bb1c007c29865a370ba420237
sarab1016 <138158021+sarab1016@users.noreply.github.com>
2023-07-08 20:17:42 +0530

    Added app.js

---
+++ app.js
@@ -0,0 +1,57 @@
+const UpperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const LowerSet = "abcdefghijklmnopqrstuvwxyz"
+const numberSet = "1234567890"
+const symbolSet = "!@#$%^&*()_+{}|:?><[]\';.,/'"
+
+// selectors
+const passBox = document.getElementById("pass-box")
+const totalChar = document.getElementById("total-char")
+const upperInput = document.getElementById("upper-case")
+const lowerInput = document.getElementById("lower-case")
+const numberInput = document.getElementById("numbers")
+const symbolInput = document.getElementById("symbols")
+
+
+
+
+const getRandomData = (dataSet) => {
+    return dataSet[Math.floor(Math.random() * dataSet.length)]
+}
+
+const generatePassword = (password = "") => {
+    if (upperInput.checked) {
+        password += getRandomData(UpperSet)
+    }
+    if (lowerInput.checked) {
+        password += getRandomData(LowerSet)
+    }
+    if (numberInput.checked) {
+        password += getRandomData(numberSet)
+    }
+    if (symbolInput.checked) {
+        password += getRandomData(symbolSet)
+    }
+    if (password.length < totalChar.value) {
+        return generatePassword(password)
+    }
+
+    passBox.innerText = truncateString(password, totalChar.value);
+}
+
+generatePassword();
+
+document.getElementById("btn").addEventListener(
+    "click",
+    function () {
+        generatePassword();
+    }
+)
+
+function truncateString(str, num) {
+    if (str.length > num) {
+        let subStr = str.substring(0, num);
+        return subStr;
+    } else {
+        return str;
+    }
+}
